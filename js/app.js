83cc1db80379c656bf84c3e7d61e72b6b355c3a5
Gabriel Patara <patara@pop-os.localdomain>
2023-12-19 13:20:02 -0300

    Ajustes feitos

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-window.onload = function(){
+
     var up = 38,down = 40,left = 37,right = 39;
     var cnv = document.querySelector("canvas");
     var ctx = cnv.getContext("2d");
     var spriteSheet = new Image();
-    spriteSheet.src = "./img/download.jpg";
+    spriteSheet.src = "./img/download.png";
     var p1 = new spritedown(spriteSheet);
 
     window.addEventListener('keydown',keydownHandler);
@@ -48,10 +48,10 @@ window.onload = function(){
                 p1.moveesquerda = false;
                 break;
             case up:
-                p1.movecima = false;
+                p1.movecima = false;    
                 break;
             case down:
-                p1.movebaixo = false;
+                p1.movebaixo = false;               
                 break;
         }
     }
@@ -67,7 +67,7 @@ window.onload = function(){
     }
     function draw(){
         ctx.clearRect(0,0,cnv.width,cnv.height);
-        p1.draw(ctx);
+        p1.draw(ctx);    
     }
 
     function loop(){
@@ -76,10 +76,6 @@ window.onload = function(){
         draw();
     }
 
-
-}
-//=================================================================================
-
 function spritedown(img){
     this.movedireita = this.moveesquerda = this.movecima = movebaixo = false;
     this.srcX = this.srcY = 0;
@@ -87,12 +83,17 @@ function spritedown(img){
     this.height = 64;
     this.posX = this.posY = 0;
     this.img = img;
-    this.speed = 1;
+    this.speed = 3;
     this.countAnim = 0;
+    var cnv = document.querySelector("canvas");
     
+
+
+
     this.draw = function(ctx){
         ctx.drawImage(this.img,this.srcX,this.srcY,this.width,this.height,this.posX,this.posY,this.width,this.height);
         this.animation();
+        
     }
 
     this.move = function(){
@@ -112,6 +113,18 @@ function spritedown(img){
             this.posY += this.speed;
             this.srcY = this.height * 0;
         }
+
+
+        // LIMITE DO MAPA
+        if(this.posX < 0){this.posX = 0}
+        if(this.posX+64 > cnv.width){this.posX = cnv.width-64}
+
+        if(this.posY < 0){this.posY = 0}
+        if(this.posY+64>cnv.height){this.posY = cnv.height-64}
+
+        // COLISÃO
+        // ctx.fillRect(90,100,190,290)
+        
     }
     this.animation = function(){
         if(this.movebaixo || this.movecima || this.movedireita || this.moveesquerda){
@@ -122,4 +135,58 @@ function spritedown(img){
             this.srcX =  Math.floor(this.countAnim / 10) * this.width;
     }
 }
+}
+
+async function movimento(key,tempo){
+    let tempoMilisegundos = tempo*1000;
+    
+
+    switch(key){
+        case right:
+            p1.movedireita = true;
+            p1.moveesquerda = false;
+            p1.movecima = false;
+            p1.movebaixo = false;
+            break;
+        case left:
+            p1.movedireita = false;
+            p1.moveesquerda = true;
+            p1.movecima = false;
+            p1.movebaixo = false;
+            break;
+        case up:
+            p1.movedireita = false;
+            p1.moveesquerda = false;
+            p1.movecima = true;
+            p1.movebaixo = false;
+            break;
+        case down:
+            p1.movedireita = false;
+            p1.moveesquerda = false;
+            p1.movecima = false;
+            p1.movebaixo = true;
+            break;
+    }
+    setTimeout(()=>{
+        switch(key){
+            case right:
+                p1.movedireita = false;
+                break;
+            case left:
+                p1.moveesquerda = false;
+                break;
+            case up:
+                p1.movecima = false;
+                break;
+            case down:
+                p1.movebaixo = false;
+                break;
+        }
+    },tempoMilisegundos);
+    await new Promise(resolve => setTimeout(resolve,tempoMilisegundos))
+}
+
+window.onload = async function (){
+   await movimento(39, 1.9);
+   await movimento(40, 1.8);
 }
