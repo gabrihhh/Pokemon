facf0fd571455c1b352e512c0c90cce5eba0d864
Gabriel Patara <patara@pop-os.localdomain>
2024-01-10 12:36:32 -0300

    colisao de objeto

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 
-    var up = 38,down = 40,left = 37,right = 39;
-    var cnv = document.querySelector("canvas");
-    var ctx = cnv.getContext("2d");
-    var spriteSheet = new Image();
-    spriteSheet.src = "./img/download.png";
-    var p1 = new spritedown(spriteSheet);
+var up = 38,down = 40,left = 37,right = 39;
+var cnv = document.querySelector("canvas"); var ctx = cnv.getContext("2d");
+var spriteSheet = new Image();
+spriteSheet.src = "./img/download.png";
+var p1 = new spritedown(spriteSheet);
 
     window.addEventListener('keydown',keydownHandler);
     window.addEventListener('keyup',keyupHandler);
@@ -63,11 +62,13 @@
         loop();
     }
     function update(){
+    
         p1.move();
     }
     function draw(){
         ctx.clearRect(0,0,cnv.width,cnv.height);
-        p1.draw(ctx);    
+        p1.draw(ctx);  
+               
     }
 
     function loop(){
@@ -76,40 +77,69 @@
         draw();
     }
 
-function spritedown(img){
-    this.movedireita = this.moveesquerda = this.movecima = movebaixo = false;
-    this.srcX = this.srcY = 0;
-    this.width = 64;
-    this.height = 64;
-    this.posX = this.posY = 0;
-    this.img = img;
-    this.speed = 3;
-    this.countAnim = 0;
-    var cnv = document.querySelector("canvas");
+    function spritedown(img){
+        this.posLastX = this.posLastY = 0;
+        this.movedireita = this.moveesquerda = this.movecima = movebaixo = false;
+        this.srcX = this.srcY = 0;
+        this.width = 64;
+        this.height = 64;
+        this.posX = this.posY = 0;
+        this.img = img;
+        this.speed = 3;
+        this.countAnim = 0;
+        var cnv = document.querySelector("canvas");
     
+    window.addEventListener('keyup',(e)=>{
+        if(e.key == 'ArrowRight'){
+            this.srcX = 0;
+            this.srcY = this.height * 2
+        }
+        if(e.key == 'ArrowDown'){
+            this.srcX = 0;
+            this.srcY = this.height * 0
+        }
+        if(e.key == 'ArrowUp'){
+            this.srcX = 0;
+            this.srcY = this.height * 3
+        }
+        if(e.key == 'ArrowLeft'){
+            this.srcX = 0;
+            this.srcY = this.height * 1
+        }
+    })
 
-
+    this.movimentation = function(horizonal,posDestino){
+        if(horizonal){
+            
+         this.movedireita = true
+            
+        }
+        
+    }
 
     this.draw = function(ctx){
         ctx.drawImage(this.img,this.srcX,this.srcY,this.width,this.height,this.posX,this.posY,this.width,this.height);
         this.animation();
-        
     }
 
     this.move = function(){
         if(this.moveesquerda){
+            this.posLastX = this.posX
             this.posX -= this.speed;
             this.srcY = this.height * 1;
         }
         if(this.movedireita){
+            this.posLastX = this.posX
             this.posX += this.speed;
             this.srcY = this.height * 2;
         }
         if(this.movecima){
+            this.posLastY = this.posY
             this.posY -= this.speed;
             this.srcY = this.height * 3
         }
         if(this.movebaixo){
+            this.posLastY = this.posY
             this.posY += this.speed;
             this.srcY = this.height * 0;
         }
@@ -123,8 +153,14 @@ function spritedown(img){
         if(this.posY+64>cnv.height){this.posY = cnv.height-64}
 
         // COLISÃO
-        // ctx.fillRect(90,100,190,290)
+        // ctx.fillRect(90,100,190,290); 
+        if(this.posX+64 > 90 && this.posX < 280-15 && this.posY + 64 > 100 && this.posY + 64< 390){
+            this.posX = this.posLastX
+            this.posY = this.posLastY
+
+        }
         
+        console.log(this.posX,this.posLastX,this.posY,this.posLastY)
     }
     this.animation = function(){
         if(this.movebaixo || this.movecima || this.movedireita || this.moveesquerda){
@@ -133,60 +169,6 @@ function spritedown(img){
                 this.countAnim = 0;
             }
             this.srcX =  Math.floor(this.countAnim / 10) * this.width;
-    }
-}
-}
-
-async function movimento(key,tempo){
-    let tempoMilisegundos = tempo*1000;
-    
-
-    switch(key){
-        case right:
-            p1.movedireita = true;
-            p1.moveesquerda = false;
-            p1.movecima = false;
-            p1.movebaixo = false;
-            break;
-        case left:
-            p1.movedireita = false;
-            p1.moveesquerda = true;
-            p1.movecima = false;
-            p1.movebaixo = false;
-            break;
-        case up:
-            p1.movedireita = false;
-            p1.moveesquerda = false;
-            p1.movecima = true;
-            p1.movebaixo = false;
-            break;
-        case down:
-            p1.movedireita = false;
-            p1.moveesquerda = false;
-            p1.movecima = false;
-            p1.movebaixo = true;
-            break;
-    }
-    setTimeout(()=>{
-        switch(key){
-            case right:
-                p1.movedireita = false;
-                break;
-            case left:
-                p1.moveesquerda = false;
-                break;
-            case up:
-                p1.movecima = false;
-                break;
-            case down:
-                p1.movebaixo = false;
-                break;
         }
-    },tempoMilisegundos);
-    await new Promise(resolve => setTimeout(resolve,tempoMilisegundos))
-}
-
-window.onload = async function (){
-   await movimento(39, 1.9);
-   await movimento(40, 1.8);
+    }
 }
